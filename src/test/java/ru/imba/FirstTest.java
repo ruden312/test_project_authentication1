6b28a746e5e04bdca4afd99dcb5ad8d3ba64f253
arudenko <rudenko_312@mail.ru>
2020-12-08 23:51:04 +0600

    Initial commit

---
+++ src/test/java/ru/imba/FirstTest.java
@@ -0,0 +1,32 @@
+package ru.imba;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstTest {
+    public ChromeDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Asus\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void firstTest() {
+        driver.get("https://imba.ru/");
+
+        String title = driver.getTitle();
+        Assert.assertTrue(title.equals("IMBA — Академия цифрового бизнеса Ingate"));
+
+        WebElement button1 = driver.findElement(By.xpath("//*[@id=\"phone-block\"]/div[2]/a"));
+        WebElement button2 = driver.findElement(By.xpath("//*[@id=\"phone-block\"]/div[3]/a"));
+
+        System.out.println(button1.isEnabled());
+        System.out.println(button2.isEnabled());
+    }
+}
